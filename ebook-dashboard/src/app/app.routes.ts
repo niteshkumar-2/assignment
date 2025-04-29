3e663f45bbe1bca428d89e0ff8e1950413667210
Nitesh Kumar <niteshsingh5121@gmail.com>
2025-04-29 11:44:49 +0530

    did code clean up

--- ebook-dashboard/src/app/app.routes.ts
+++ ebook-dashboard/src/app/app.routes.ts
@@ -5,31 +5,23 @@ import { AuthGuard } from './core/guards/auth.guard';
 import { BookReaderComponent } from './dashboard/book-reader/book-reader.component';
 import { BookProgressComponent } from './dashboard/book-progress/book-progress.component';
 
-
 export const routes: Routes = [
-  // Default route redirects to the login page
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  
-  // Login page
   { path: 'login', component: LoginComponent },
-  
-  // Dashboard, protected by AuthGuard
-  { 
-    path: 'dashboard', 
-    component: DashboardComponent, 
-    canActivate: [AuthGuard], // Only accessible if the user is authenticated
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
   },
-  
-  // Book Reader page, protected by AuthGuard
-  { 
-    path: 'book-reader', 
-    component: BookReaderComponent, 
-    canActivate: [AuthGuard], 
+  {
+    path: 'book-reader',
+    component: BookReaderComponent,
+    canActivate: [AuthGuard],
   },
-  
-  { 
-    path: 'book-progress', 
-    component: BookProgressComponent, 
-    canActivate: [AuthGuard], 
-  }
+
+  {
+    path: 'book-progress',
+    component: BookProgressComponent,
+    canActivate: [AuthGuard],
+  },
 ];
